83fde3bb429a81999d1368ae0e00b7c72b3b10fe
AlexVerine <alexverine@me.com>
2024-10-11 12:54:54 +0200

    update scripts

--- scripts/launch_embeddings_single_v3.sh
+++ scripts/launch_embeddings_single_v3.sh
@@ -1,13 +1,15 @@
 job="knowledge_embeddings"
 job_name=$job 
 project_path="/lustre/fswork/projects/rech/esq/udg63qz/knowladge/knowledge_processing"
+# Check if the tar files path is provided as an argument
 if [ $# -eq 0 ]; then
     echo "Error: Please provide the path to tar files as an argument."
     echo "Usage: $0 <path_to_tar_files>"
     exit 1
 fi
-tar_files_path="$1"
+tar_files_path="${1%/}"
 tar_file="$tar_files_path/data-000001-000186.tar"
+
 out_dir="${tar_files_path}_embeddings"
 
 echo "Launching job $job_name for single tar file"
@@ -16,13 +18,13 @@ sbatch <<EOT
 #SBATCH --job-name=$job_name
 #SBATCH --account=tqh@a100
 #SBATCH --partition=gpu_p5
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --constraint=a100
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --output=$project_path/knowledge_processing/slurm_outputs/$job_name/%j_out.txt
-#SBATCH --error=$project_path/knowledge_processing/slurm_outputs/$job_name/%j_err.txt
+#SBATCH --output=$project_path/slurm_outputs/%j_out.txt
+#SBATCH --error=$project_path/slurm_outputs/%j_err.txt
 #SBATCH --time=00:30:00
 #SBATCH --hint=nomultithread
 
@@ -34,13 +36,12 @@ HF_DATASETS_OFFLINE=1
 HF_HUB_OFFLINE=1
 TRANSFORMERS_OFFLINE=1
 
-# Echo des commandes lancees
 set -x 
 
 srun python $project_path/main.py \
     --mode embedding \
     --tar_file $tar_file \
     --output_dir $out_dir \
-    --ngpus 4 \
+    --ngpus 1 \
     --batch_size 1024
 EOT
--- scripts/launch_embeddings_v3.sh
+++ scripts/launch_embeddings_v3.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 job="knowledge_embeddings"
 job_name=$job 
-project_path="/lustre/fswork/projects/rech/esq/udg63qz/knowladge"
+project_path="/lustre/fswork/projects/rech/esq/udg63qz/knowladge/knowledge_processing"
 # Check if the tar files path is provided as an argument
 if [ $# -eq 0 ]; then
     echo "Error: Please provide the path to tar files as an argument."
     echo "Usage: $0 <path_to_tar_files>"
     exit 1
 fi
-tar_files_path="$1"
+tar_files_path="${1%/}"
 tar_files=($tar_files_path/data*.tar)
 num_files=${#tar_files[@]}
 out_dir="${tar_files_path}_embeddings"
@@ -18,14 +19,14 @@ sbatch <<EOT
 #SBATCH --job-name=$job_name
 #SBATCH --account=tqh@a100
 #SBATCH --partition=gpu_p5
-#SBATCH --gres=gpu:6
+#SBATCH --gres=gpu:1
 #SBATCH --constraint=a100
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --output=$project_path/knowledge_processing/slurm_outputs/$job_name/%A_%a_out.txt
-#SBATCH --error=$project_path/knowledge_processing/slurm_outputs/$job_name/%A_%a_err.txt
-#SBATCH --time=02:00:00
+#SBATCH --output=$project_path/slurm_outputs/%A_%a_out.txt
+#SBATCH --error=$project_path/slurm_outputs/%A_%a_err.txt
+#SBATCH --time=00:30:00
 #SBATCH --hint=nomultithread
 #SBATCH --array=0-$((num_files - 1))
 
@@ -37,7 +38,6 @@ HF_DATASETS_OFFLINE=1
 HF_HUB_OFFLINE=1
 TRANSFORMERS_OFFLINE=1
 
-# Echo des commandes lancees
 set -x 
 
 # Get the current tar file based on the array task ID
@@ -47,6 +47,6 @@ srun python $project_path/main.py \
     --mode embedding \
     --tar_file $current_tar_file \
     --output_dir $out_dir \
-    --ngpus 4 \
+    --ngpus 1 \
     --batch_size 1024
 EOT
