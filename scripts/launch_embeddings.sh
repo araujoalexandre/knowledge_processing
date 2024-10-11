8f568596471d15a9a5e687c2d3946c7c8f66af51
AlexVerine <alexverine@me.com>
2024-10-11 13:15:05 +0200

    clean up scripts

--- scripts/launch_embeddings.sh
+++ scripts/launch_embeddings.sh
@@ -1,27 +1,54 @@
+#!/bin/bash
 job="knowledge_embeddings"
 job_name=$job 
-out_dir="slurm_outputs"
-          echo "Launching job $job_name"
-        sbatch <<EOT
+project_path="/lustre/fswork/projects/rech/esq/udg63qz/knowladge/knowledge_processing"
+# Check if the tar files path is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Error: Please provide the path to tar files as an argument."
+    echo "Usage: $0 <path_to_tar_files>"
+    exit 1
+fi
+tar_files_path="${1%/}"
+tar_files=($tar_files_path/data*.tar)
+num_files=${#tar_files[@]}
+out_dir="${tar_files_path}_embeddings"
+
+echo "Launching job array $job_name with $num_files tasks"
+sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=$job_name
-#SBATCH -A tqh@a100
-#SBATCH -p gpu_p5
-#SBATCH --gres=gpu:6
-#SBATCH -C a100
+#SBATCH --account=tqh@a100
+#SBATCH --partition=gpu_p5
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=a100
 #SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=6
-#SBATCH --output=$out_dir/$job_name/%t_out.txt
-#SBATCH --error=$out_dir/$job_name/%t_err.txt
-#SBATCH --time=20:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=$project_path/slurm_outputs/%A_%a_out.txt
+#SBATCH --error=$project_path/slurm_outputs/%A_%a_err.txt
+#SBATCH --time=00:30:00
 #SBATCH --hint=nomultithread
+#SBATCH --array=0-$((num_files - 1))
 
 module purge 
-module load cpuarch/amd
-module load pytorch-gpu/py3/1.12.1
-HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1
-# Echo des commandes lancees
+module load arch/a100
+module load pytorch-gpu/py3/2.3.0
+
+HF_DATASETS_OFFLINE=1
+HF_HUB_OFFLINE=1
+TRANSFORMERS_OFFLINE=1
+
 set -x 
-srun python embeddings/create_embeddings_distributed.py --input_dir datadir/wikiprocessed --output_dir datadir/wikiembeddings --batch_size 1024
+
+# Get the current tar file based on the array task ID
+tar_files=($tar_files_path/data*.tar)
+current_tar_file=\${tar_files[\$SLURM_ARRAY_TASK_ID]}
+
+srun python $project_path/main.py \\
+    --mode embedding \\
+    --tar_file \$current_tar_file \\
+    --output_dir $out_dir \\
+    --ngpus 1 \\
+    --batch_size 1024
 EOT
+
